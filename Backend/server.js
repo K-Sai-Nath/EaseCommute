d89bee91ecddd6744763fe4e1e87ad9e6e3f762c
K-Sai-Nath <sainathkancharakuntla@gmail.com>
2025-08-23 13:13:54 +0530

    Updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,28 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const authRoutes=require("./routes/AuthRoutes")
-const app=express();
-const cors=require('cors');
-app.use(cors());
-app.use(express.json())
-app.use("/api/auth",authRoutes);
-require('dotenv').config();
-mongoose.connect(process.env.MONGO_DB_URL,{useNewUrlParser:true,useUnifiedTopology: true })
-.then(()=>{
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/AuthRoutes");
+const app = express();
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+require("dotenv").config();
+mongoose
+  .connect(process.env.MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("DB Connected");
-})
-.catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 const PORT = process.env.PORT || 9000;
-app.listen(PORT,()=>{
-    console.log("Server Started");
-})
+app.listen(PORT, () => {
+  console.log("Server Started");
+});
